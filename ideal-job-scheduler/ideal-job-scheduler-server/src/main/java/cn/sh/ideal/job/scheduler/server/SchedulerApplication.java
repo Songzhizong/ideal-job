31b9bdc61cbe858fd0a00f79764031430fb550d5
宋志宗 <18256928780@189.cn>
2020-08-26 22:15:19 +0800

    update

--- ideal-job-scheduler/ideal-job-scheduler-server/src/main/java/cn/sh/ideal/job/scheduler/server/SchedulerApplication.java
+++ ideal-job-scheduler/ideal-job-scheduler-server/src/main/java/cn/sh/ideal/job/scheduler/server/SchedulerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -16,7 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @EnableScheduling
 @EnableJpaAuditing
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"cn.sh.ideal.job.scheduler.core"})
 @EntityScan(basePackages = {"cn.sh.ideal.job.scheduler.core"})
